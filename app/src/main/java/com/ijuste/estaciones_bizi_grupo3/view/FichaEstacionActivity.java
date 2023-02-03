1aea10e376c9dc576dd79a0ba59ccea38a82619b
Snowboys <ismaeljg2002@gmail.com>
2023-02-03 19:10:20 +0100

    ActivityFichaEstacion

---
+++ app/src/main/java/com/ijuste/estaciones_bizi_grupo3/view/FichaEstacionActivity.java
@@ -0,0 +1,16 @@
+package com.ijuste.estaciones_bizi_grupo3.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ijuste.estaciones_bizi_grupo3.R;
+
+public class FichaEstacionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ficha_estacion);
+    }
+}

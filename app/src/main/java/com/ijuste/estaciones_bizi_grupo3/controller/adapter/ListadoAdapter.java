347f96d36f4df5039f013178291c516a7727a6ac
Alvaro <alvaropuyobraulio@gmail.com>
2023-02-06 15:52:01 +0100

    Adapter, Recycler, listado...

--- app/src/main/java/com/ijuste/estaciones_bizi_grupo3/controller/adapter/ListadoAdapter.java
+++ app/src/main/java/com/ijuste/estaciones_bizi_grupo3/controller/adapter/ListadoAdapter.java
@@ -1,4 +1,68 @@
 package com.ijuste.estaciones_bizi_grupo3.controller.adapter;
 
-public class ListadoAdapter {
+import android.app.AlertDialog;
+import android.text.NoCopySpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ijuste.estaciones_bizi_grupo3.R;
+import com.ijuste.estaciones_bizi_grupo3.model.EstacionBici;
+
+import java.util.List;
+
+public class ListadoAdapter extends RecyclerView.Adapter<ListadoAdapter.ViewHolder> {
+
+    private final List<EstacionBici> estaciones;
+
+    public ListadoAdapter(List<EstacionBici> estaciones) {this.estaciones = estaciones;}
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.card_layout, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        EstacionBici estacion = estaciones.get(position);
+
+        holder.txtTituloCalle.setText(estacion.getAddress());
+        holder.txtBicisDisponibles.setText(String.valueOf(estacion.getBicisDisponibles()));
+        holder.txtAnclajesDisponibles.setText(String.valueOf(estacion.getAnclajesDisponibles()));
+
+
+        holder.itemView.setOnClickListener(v->{
+
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return estaciones.size();
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtTituloCalle;
+        TextView txtBicisDisponibles;
+        TextView txtAnclajesDisponibles;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtTituloCalle = itemView.findViewById(R.id.txtTituloCalle);
+            txtBicisDisponibles = itemView.findViewById(R.id.txtBicisDisponibles);
+            txtAnclajesDisponibles = itemView.findViewById(R.id.txtAnclajesDisponibles);
+        }
+    }
 }
--- app/src/main/java/com/ijuste/estaciones_bizi_grupo3/view/ListadoActivity.java
+++ app/src/main/java/com/ijuste/estaciones_bizi_grupo3/view/ListadoActivity.java
@@ -1,16 +1,36 @@
 package com.ijuste.estaciones_bizi_grupo3.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.ijuste.estaciones_bizi_grupo3.R;
+import com.ijuste.estaciones_bizi_grupo3.bbdd.EstacionesADO;
+import com.ijuste.estaciones_bizi_grupo3.controller.MenuController;
+import com.ijuste.estaciones_bizi_grupo3.controller.adapter.ListadoAdapter;
+import com.ijuste.estaciones_bizi_grupo3.model.EstacionBici;
 
-public class ListadoActivity extends AppCompatActivity {
+import java.util.List;
+
+public class ListadoActivity extends MenuController {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listado);
+
+        RecyclerView recycler = findViewById(R.id.recyclerLista);
+        recycler.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 1);
+        recycler.setLayoutManager(layoutManager);
+
+        EstacionesADO objetoAdo = new EstacionesADO(this);
+
+        List<EstacionBici> estaciones = objetoAdo.getAll();
+        recycler.setAdapter(new ListadoAdapter(estaciones));
+
     }
 }

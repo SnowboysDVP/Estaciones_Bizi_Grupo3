4b87542df3247f24359c85b0a369556923a481ad
vvinerta <vinertacrespo@gmail.com>
2023-02-08 16:46:35 +0100

    Pinceladas de codigo

--- app/src/main/java/com/ijuste/estaciones_bizi_grupo3/view/FichaEstacionActivity.java
+++ app/src/main/java/com/ijuste/estaciones_bizi_grupo3/view/FichaEstacionActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ijuste.estaciones_bizi_grupo3.R;
 import com.ijuste.estaciones_bizi_grupo3.bbdd.EstacionesADO;
@@ -31,52 +32,42 @@ public class FichaEstacionActivity extends AppCompatActivity implements View.OnC
         setContentView(R.layout.activity_ficha_estacion);
 
         iniciarComponentes(textviews,nombretextview,botones,nombrebotones);
-        EstacionesADO miestaciones= new EstacionesADO(this);
-        lista= miestaciones.getAll();
-        Intent intento= new Intent();
-        posicion=intento.getIntExtra("posicion",0);
 
+        Intent intento= getIntent();
+        posicion=intento.getIntExtra("posicion",0);
+        lista = (List<EstacionBici>) intento.getSerializableExtra("array");
+
+        mostrarDatos();
+
+        nombrebotones[0].setOnClickListener(this::onClick);
+        nombrebotones[1].setOnClickListener(this::onClick);
 
 
     }
 
     @Override
     public void onClick(View view) {
+
         if (view == nombrebotones[0]) {
+            if(posicion==0){
+                aviso();
+            }else{
+                posicion--;
+            }
             // Atras
-            posicion--;
-        }
-        else if (view == nombrebotones[1]) {
-            // Adelante
-            posicion++;
-        }
-        if(posicion==nombretextview.length){
-            nombrebotones[1].setEnabled(false);
-        }else if(posicion==0){
-            nombrebotones[0].setEnabled(false);
-        }else{
-            nombrebotones[0].setEnabled(true);
-            nombrebotones[1].setEnabled(true);
+
+        }else if (view == nombrebotones[1]) {
+            if(posicion==lista.size()-1){
+                aviso();
+            }else {
+                // Adelante
+                posicion++;
+            }
         }
 
-            nombretextview[0].setText(String.valueOf(lista.get(posicion).getId()));
-            nombretextview[1].setText(String.valueOf(lista.get(posicion).getAbout()));
-            nombretextview[2].setText(String.valueOf(lista.get(posicion).getTitle()));
-            nombretextview[3].setText(String.valueOf(lista.get(posicion).getEstado()));
-            nombretextview[4].setText(String.valueOf(lista.get(posicion).getEstadoEstacion()));
-            nombretextview[5].setText(String.valueOf(lista.get(posicion).getAddress()));
-            nombretextview[6].setText(String.valueOf(lista.get(posicion).getTipoEquipamiento()));
-            nombretextview[7].setText(String.valueOf(lista.get(posicion).getBicisDisponibles()));
-            nombretextview[8].setText(String.valueOf(lista.get(posicion).getAnclajesDisponibles()));
-            nombretextview[9].setText(String.valueOf(lista.get(posicion).getLastUpdated()));
-            nombretextview[10].setText(String.valueOf(lista.get(posicion).getDescription()));
-            nombretextview[11].setText(String.valueOf(lista.get(posicion).getDescripcion()));
-            nombretextview[12].setText(String.valueOf(lista.get(posicion).getIcon()));
-            nombretextview[13].setText(String.valueOf(lista.get(posicion).getGeometry().getType()));
-            nombretextview[14].setText(String.valueOf(lista.get(posicion).getGeometry().getCoordinates()));
-
-
+            mostrarDatos();
     }
+
     private void iniciarComponentes(int[]idview, TextView[]nombreview, int[]idimagen, ImageView[]imagenes){
 
         for(int i=0;i< idview.length;i++){
@@ -85,5 +76,27 @@ public class FichaEstacionActivity extends AppCompatActivity implements View.OnC
         for(int x=0;x<imagenes.length;x++){
             imagenes[x]=findViewById(idimagen[x]);
         }
+    }
+
+    private void mostrarDatos(){
+        nombretextview[0].setText(String.valueOf(lista.get(posicion).getId()));
+        nombretextview[1].setText(lista.get(posicion).getAbout());
+        nombretextview[2].setText(lista.get(posicion).getTitle());
+        nombretextview[3].setText(lista.get(posicion).getEstado());
+        nombretextview[4].setText(lista.get(posicion).getEstadoEstacion());
+        nombretextview[5].setText(lista.get(posicion).getAddress());
+        nombretextview[6].setText(lista.get(posicion).getTipoEquipamiento());
+        nombretextview[7].setText(String.valueOf(lista.get(posicion).getBicisDisponibles()));
+        nombretextview[8].setText(String.valueOf(lista.get(posicion).getAnclajesDisponibles()));
+        nombretextview[9].setText(lista.get(posicion).getLastUpdated());
+        nombretextview[10].setText(lista.get(posicion).getDescription());
+        nombretextview[11].setText(lista.get(posicion).getDescripcion());
+        nombretextview[12].setText(lista.get(posicion).getIcon());
+        nombretextview[13].setText(lista.get(posicion).getGeometry().getType());
+        nombretextview[14].setText(lista.get(posicion).getGeometry().getCoordinates()[0]+" "+
+                                    lista.get(posicion).getGeometry().getCoordinates()[1]);
+    }
+    private void aviso(){
+        Toast.makeText(this, "No hay mas registros", Toast.LENGTH_LONG).show();
     }
 }
